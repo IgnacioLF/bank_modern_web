662113a739f256a83d3f0e0941d2f60cd62d215d
IgnacioLF <ignaciolopezfeliu@gmail.com>
2022-09-11 21:37:45 +0200

    -added footer CTA clients

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,10 +2,10 @@ import styles from '../style'
 import { arrowUp } from '../assets'
 
 const GetStarted = () => {
-    //  TODO add hover
+
     return(
         <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-            <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={`${styles.flexCenter} flex-col hover:bg-[#3F3E45] bg-primary w-[100%] h-[100%] rounded-full`}>
                 <div className={`${styles.flexStart} flex-row`}>
                     <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
                         <span className='text-gradient'>
