4c3aecc926708dcc7ae852d5139d88b7c75a889c
IgnacioLF <ignaciolopezfeliu@gmail.com>
2022-09-11 18:31:58 +0200

    -added billling

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,26 @@
+import { apple, bill, google} from "../assets"
+import styles, { layout } from "../style"
+
 const Billing = () => {
     return(
-        <div>
-            Billing
-        </div>
+        <section id="product" className={layout.sectionReverse}>
+            <div className={layout.sectionImgReverse}>
+                <img src={bill} alt="bill" className="w-[100%] h-[100%] relative z-[5]" />
+
+                <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] white__gradient rounded-full" />
+                <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] pink__gradient rounded-full" />
+            </div>
+
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>Easily control your billing <br className="sm:block hidden" /> & invoicing.</h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+
+                <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+                    <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+                    <img src={google} alt="play_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+                </div>
+            </div>
+        </section>
     )
 }
 
